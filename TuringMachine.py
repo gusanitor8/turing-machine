d8bf1e60ec011c5f1713a7495a3a364bbdfb1f6d
gusanitor8 <gusanitor8@gmail.com>
2023-11-18 20:09:43 -0600

    Avances en la maquina de Turing, implementacion de clase dedicada para la cinta

--- TuringMachine.py
+++ TuringMachine.py
@@ -1,5 +1,7 @@
 L = -1
 R = 1
+S = 0
+
 
 class TuringMachine:
     def __init__(self, alphabet: set, blank_symbol: str, input_symbols: set, states: set, initial_state: str,
@@ -22,9 +24,84 @@ class TuringMachine:
         self.accepting_states = accepting_states
         self.transition_function = transition_function
 
-        # TODO the machine must read blank if: tape is empty, tape is not empty and head is at the end of the tape
-        self.tape = ""  # Tape is set in run method
+        self.tape = None  # Tape is set in run method
         self.cache = ""
+        self.current_state = initial_state
 
     def run(self, input_string: str):
-        self.tape = self.blank_symbol + input_string + self.blank_symbol
+        self.tape = Tape(input_string, self.blank_symbol)
+
+        while self.current_state not in self.accepting_states:
+            curr_char = self.tape.get_current()
+            new_state, new_cache, tape_output, head_direction = self.get_transition(self.current_state, curr_char,
+                                                                                    self.cache)
+            self.current_state = new_state
+            self.cache = new_cache
+            self.tape.write(tape_output)
+
+            if head_direction == R:
+                self.tape.go_right()
+            elif head_direction == L:
+                self.tape.go_left()
+
+    def get_transition(self, state, tape_value, cache_value):
+        if ((state, cache_value), tape_value) in self.transition_function:
+            transition = self.transition_function[((state, cache_value), tape_value)]
+            try:
+                new_state = transition[0][0]
+                new_cache = transition[0][1]
+                tape_output = transition[1]
+                head_direction = transition[2]
+            except IndexError:
+                exit("Transition function is not in the correct format")
+
+            return new_state, new_cache, tape_output, head_direction
+        else:
+            exit("No transition found for state: " + state + " tape_value: " + tape_value + " cache_value: "
+                 + cache_value)
+
+
+class Node:
+    def __init__(self, value):
+        self.char = ""
+        self.next = None
+        self.prev = None
+
+
+class Tape:
+    def __init__(self, string, blank_symbol):
+        self.blank_symbol = blank_symbol
+        self.current = None
+
+        self._set_initial_node(string[0])
+        for char in string[1:]:
+            self._set_next(char)
+
+    def go_right(self):
+        if self.current.next is None:
+            self._set_next(self.blank_symbol)
+            self.current = self.current.next
+        else:
+            self.current = self.current.next
+
+    def go_left(self):
+        if self.current.prev is None:
+            self._set_prev(self.blank_symbol)
+            self.current = self.current.prev
+        else:
+            self.current = self.current.prev
+
+    def write(self, value):
+        self.current.char = value
+
+    def get_current(self):
+        return self.current.char
+
+    def _set_initial_node(self, value):
+        self.current = Node(value)
+
+    def _set_next(self, value):
+        self.current.next = Node(value)
+
+    def _set_prev(self, value):
+        self.current.prev = Node(value)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from TuringMachine import TuringMachine, L, R
+from TuringMachine import TuringMachine, L, R, S
 
 transition_function = {
-    (("q0", "B"), "0"): ("q0", "0", R),
-
+    # ((current_state, cache),tape_value_input): ((new_state, new_cache), tape_output, head_directionRSL )
+    (("0", "_"), "a"): (("1", "a"), "_", R),
+    (("0", "_"), "b"): (("1", "b"), "_", R),
 }

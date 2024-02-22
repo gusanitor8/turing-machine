9e924e1892d87ecc501fbf260e9e89596ba6fa60
María Marta Ramírez Gil <ram21342@uvg.edu.gt>
2024-02-22 13:19:18 -0600

    First Try Fibonacci TM

--- main.py
+++ main.py
@@ -4,23 +4,23 @@ from TuringMachine import TuringMachine
 
 def main():
     # Turing machine 1
-    print("First Turing Machine: Reconocedora")
+    print("Fibonacci Turing Machine")
     alphabet, input_symbols, states, initial_state, accepting_states, transition_function, simulation_strings = (
-        get_turing_machine_attr('MT_reconocedora.yaml'))
+        get_turing_machine_attr('MT_fibonacci.yaml'))
 
     for string in simulation_strings:
         tm = TuringMachine(alphabet, input_symbols, states, initial_state, accepting_states, transition_function)
 
         tm.run(string)
 
-    # Turing machine 2
-    print("Second Turing Machine: Alteradora")
-    alphabet, input_symbols, states, initial_state, accepting_states, transition_function, simulation_strings = (
-        get_turing_machine_attr('MT_alteradora.yaml'))
-
-    for string in simulation_strings:
-        tm = TuringMachine(alphabet, input_symbols, states, initial_state, accepting_states, transition_function)
-        tm.run(string)
+    # # Turing machine 2
+    # print("Second Turing Machine: Alteradora")
+    # alphabet, input_symbols, states, initial_state, accepting_states, transition_function, simulation_strings = (
+    #     get_turing_machine_attr('MT_alteradora.yaml'))
+    #
+    # for string in simulation_strings:
+    #     tm = TuringMachine(alphabet, input_symbols, states, initial_state, accepting_states, transition_function)
+    #     tm.run(string)
 
 
 if __name__ == "__main__":
